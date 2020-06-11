bac7a9b57088b5d6f5bbc84ca28bd57ba5319ada
Theofani Samuel <liow.ts@gmail.com>
2020-06-11 22:01:12 +0700

    update: add StartGame View Contoller, PlayersName View Contoller, add custom fonts, buttons PNG assets, game background & overlay assets

---
+++ NanoChallenge3/StartGameVC.swift
@@ -0,0 +1,30 @@
+//
+//  StartGameVC.swift
+//  NanoChallenge3
+//
+//  Created by Theofani on 11/06/20.
+//  Copyright © 2020 Rahman Fadhil. All rights reserved.
+//
+
+import UIKit
+
+class StartGameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
