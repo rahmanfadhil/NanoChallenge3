30d5340674a73768333cad3a55658ae7f31103ac
Theofani Samuel <liow.ts@gmail.com>
2020-06-12 10:35:47 +0700

    update

--- NanoChallenge3/GameScene.swift
+++ NanoChallenge3/GameScene.swift
@@ -332,10 +332,10 @@ class GameScene: SKScene, GameDelegate {
         currentNode = nil
     }
     
-    func setPlayerNames(player1: String, player2: String) {
-        print(player1)
-        print(player2)
-    }
+//    func setPlayerNames(player1: String, player2: String) {
+//        self.player1 = player1
+//        self.player2 = player2
+//    }
     
     func changePlayer() {
         displayBlockOptions()
